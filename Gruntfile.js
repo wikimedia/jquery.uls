89d64b7e93c777ec79e65b60056351c7c5135f7f
Timo Tijhof <krinklemail@gmail.com>
2014-10-09 05:59:32 +0200

    build: Clean up Gruntfile
    * Sort jshintrc a-z and section it.
    
    * Remove grunt.js from jshintignore (file no longer exists).
    
    * Simplify jshint config by using globstar. The jshintignore
      file still applies (to skip jquery.uls.data.js).
    
    * Update jshint config to also validate Gruntfile.js
    
    * Update listed dependency on 'grunt' and 'grunt-cli' to the
      versions they currently expand to already. We don't know if
      we're compatible with older versions. '^x.y' means, equal or
      higher (within the same major release). Similar to '~x.y',
      except that tilde tolerates older versions.
    
    * Update grunt-contrib-jshint and grunt-contrib-qunit to their
      latest versions.
    
    * Remove obsolete jshintrc parsing hack, use built-in 'jshintrc'
      option as of v0.8.0.
    
    * Remove unused 'pgk' property in grunt config.
    
    * Remove unused grunt-contrib-jasmine and grunt-contrib-copy
      from devDependencies (unused as of ab9b01444f).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,22 @@
-/*!
- * Grunt file
- *
- * @package jquery.i18n
- */
-'use strict';
 /*jshint node:true */
+'use strict';
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
 
 	grunt.initConfig( {
-		pkg: grunt.file.readJSON( 'package.json' ),
 		jshint: {
-			options: JSON.parse( grunt.file.read( '.jshintrc' )
-				.replace( /\/\*(?:(?!\*\/)[\s\S])*\*\//g, '' ).replace( /\/\/[^\n\r]*/g, '' ) ),
-			all: ['src/jquery.uls.core.js',
-			'src/jquery.uls.lcd.js',
-			'src/jquery.uls.data.utils.js',
-			'src/jquery.uls.languagefilter.js',
-			'src/jquery.uls.regionfilter.js' ]
+			options: {
+				jshintrc: true
+			},
+			all: ['*.js', 'src/*.js']
 		},
 		qunit: {
-			all: ['test/index.html']
+			all: 'test/index.html'
 		}
 	} );
 
-	grunt.registerTask( 'lint', ['jshint'] );
-	grunt.registerTask( 'unit', ['qunit'] );
-	grunt.registerTask( 'test', ['lint', 'unit'] );
-	grunt.registerTask( 'default', ['test'] );
+	grunt.registerTask( 'lint', 'jshint' );
+	grunt.registerTask( 'test', ['lint', 'qunit'] );
+	grunt.registerTask( 'default', 'test' );
 };
