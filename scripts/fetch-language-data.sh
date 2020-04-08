d7cbd0a03de2e49ae1becca60bf5340e0841dd0e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2020-04-08 17:46:23 +0300

    Fixes for fetch-language-data.sh (#360)
    * Fixes for fetch-language-data.sh
    
    * Portable path function, instead of `readlink -f`, which doesn't
      work on mac.
    * Remove extra '/' character from DEST. It is added again later.
    * Change "wrote" to "written" — clearer meaning of what was done.
    
    * Update language-data
    
    * Update sjd autonym
    * Move ms-arab a few lines (automatic change from CLDR)
    
    Updating to
    https://github.com/wikimedia/language-data/commit/b485f5a1343ba6348faca8e4f4994fefe6c195e6

--- scripts/fetch-language-data.sh
+++ scripts/fetch-language-data.sh
@@ -2,8 +2,9 @@
 
 set -o errexit -o nounset -o pipefail
 
-BASEDIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-DEST="$BASEDIR/src/"
+scriptdir() { perl -MCwd -e 'print Cwd::abs_path shift;' "$1"; }
+BASEDIR=$(dirname "$(dirname "$(scriptdir "$0")")")
+DEST="$BASEDIR/src"
 CLONEDIR="$BASEDIR/vendor/language-data"
 UPSTREAM="https://github.com/wikimedia/language-data.git"
 
@@ -30,4 +31,4 @@ echo "Transforming language-data"
 
 "$BASEDIR"/node_modules/browserify/bin/cmd.js "$CLONEDIR"/data/language-data.json -t "$BASEDIR"/scripts/transform.js -o "$DEST"/jquery.uls.data.js
 
-echo "language-data wrote to $DEST/jquery.uls.data.js"
+echo "language-data written to $DEST/jquery.uls.data.js"
