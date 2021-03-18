bb343d9f42fea4df7b81632a0615c81374089e26
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-18 15:59:40 +0530

    Use sed and a template for generating jquery.uls.data.js
    Drop browserify.

--- scripts/fetch-language-data.sh
+++ scripts/fetch-language-data.sh
@@ -18,13 +18,9 @@ then (
 	git clone "$UPSTREAM" "$CLONEDIR"
 fi
 
-# This re-installs it every time, but guarantees we have the right version
-echo "Installing browserify"
 cd "$BASEDIR";
-npm install --no-save browserify@v16.5.2
-
 echo "Transforming language-data"
-
-"$BASEDIR"/node_modules/browserify/bin/cmd.js "$CLONEDIR"/data/language-data.json -t "$BASEDIR"/scripts/transform.js -o "$DEST"/jquery.uls.data.js
+JSONDATA="$CLONEDIR/data/language-data.json"
+cat "$BASEDIR"/scripts/jquery.uls.data.template.js | sed -e "/languagedata/r $JSONDATA" -e "/languagedata/d" > "$DEST"/jquery.uls.data.js
 
 echo "language-data written to $DEST/jquery.uls.data.js"
